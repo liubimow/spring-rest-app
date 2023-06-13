c0a05496e6349598a81912ce98d78638cd1405eb
LiubimoW <iilyasp83@gmail.com>
2023-06-14 03:00:04 +0500

    Add ignore for "id" field from JSON

--- src/main/kotlin/com/github/liubimow/springrestapp/DTO/Request.kt
+++ src/main/kotlin/com/github/liubimow/springrestapp/DTO/Request.kt
@@ -1,5 +1,6 @@
 package com.github.liubimow.springrestapp.DTO
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.NotBlank
 @Table(name = "requests")
 class Request (
     @Id
+    @JsonIgnore
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     @NotBlank
